3c809a323752df60c35e94442cd307b431ecd9a5
ZITOUNI Nidhal <nidhalzitouni111@gmail.com>
2026-06-20 15:32:41 +0100

    feat: initialize package with exports for mpc_python core components

--- mpc_python/__init__.py
+++ mpc_python/__init__.py
@@ -1,14 +1,22 @@
 """mpc_python package."""
 
-from .cvxpy_mpc import IterativeMPC
-from .cvxpy_mpc.utils import (
+from .cvxpy_mpc import (
+    IterativeMPC,
     load_yaml,
     wrap_angle,
     bicycle_model,
     linearize_dynamics,
+    linearize_obstacle_constraint,
     build_circular_reference,
     build_waypoint_reference,
     build_figure8_reference,
+    build_lane_change_reference,
+    compute_tracking_metrics,
+    tracking_error_stats,
+    control_effort,
+    constraint_satisfaction,
+    summarize,
+    MPCRunner,
 )
 
 __all__ = [
@@ -17,7 +25,15 @@ __all__ = [
     "wrap_angle",
     "bicycle_model",
     "linearize_dynamics",
+    "linearize_obstacle_constraint",
     "build_circular_reference",
     "build_waypoint_reference",
     "build_figure8_reference",
+    "build_lane_change_reference",
+    "compute_tracking_metrics",
+    "tracking_error_stats",
+    "control_effort",
+    "constraint_satisfaction",
+    "summarize",
+    "MPCRunner",
 ]
